e14dee17b402511124d9f1ca1e2b5aa6c42b4325
Mazen Imara <mazen.imara@gmail.com>
2017-09-18 12:54:43 +0200

    add skicka button

--- js/questionnaire.js
+++ js/questionnaire.js
@@ -38,7 +38,7 @@
       function showQuestionnaire(qnaire) {
         var part1 = '<div id="myModal" class="modal"><div class="modal-content"><form id="questionnaireForm" method="post"><div class="modal-header"><span class="close"><input type="submit" name="" value="X"></span><h4>' + qnaire.description + '</h4></div><div class="modal-body">';
         var part2 = getQuestions(qnaire.questions);
-        var part3 = '</div></form></div></div>';
+        var part3 = '<input type="submit" value="Skicka"></div></form></div></div>';
         $("body").append(part1 + part2 + part3);
         submitQuestionnaire();
       }
